606427a1954d7e1672fdc2c1d28cd6aee624ba80
Jorge De Los Santos <delossantosmfq@gmail.com>
2016-03-20 19:42:52 -0600

    Added constraint_node function

--- pyapdl/loads.py
+++ pyapdl/loads.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
+def constraint_node(node,**bc):
+    _bc = ""
+    if not bc:
+        _bc += "D,%s,ALL,,"%(node)
+    else:
+        for constraint in bc:
+            _bc += ("D,%s,%s,%g\n"%(node,constraint,bc[constraint])).upper()
+    return _bc
+
+
+
 def load_node(node,**load):
-    pass
+    _load = ""
+    if not load:
+        pass
+    else:
+        for ld in load:
+            _load += ("F,%s,%s,%g\n"%(node,ld,load[ld])).upper()
+    return _load
+
     
 def load_line(line,**load):
     pass
@@ -13,4 +31,5 @@ def load_area(area,**load):
 
 
 if __name__ == '__main__':
-    pass
+    #print constraint_node(1,ux=0,uy=0)
+    print load_node(5,fy=-1000)
